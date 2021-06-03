7e81d8e5a5ae6cec69ff639ac3b52dc4bbe40d6c
Yu357 <forestgreen14689@gmail.com>
2021-06-03 23:58:23 +0900

    ボトムナビゲーションの項目を好きに変えてみた。

---
+++ app/src/main/java/com/example/y/bottomnav02/SettingFragment.kt
@@ -0,0 +1,21 @@
+package com.example.y.bottomnav02
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.y.bottomnav02.R
+
+class SettingFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+}
