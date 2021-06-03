4a850c867141086da5bb71a8a4f0d892e32883b1
Yu357 <forestgreen14689@gmail.com>
2021-06-03 23:35:18 +0900

    無駄だと感じるコードを排除した。さっぱりしたUIに改修した。

--- app/src/main/java/com/example/y/bottomnav02/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/y/bottomnav02/ui/home/HomeFragment.kt
@@ -12,20 +12,14 @@ import com.example.y.bottomnav02.R
 
 class HomeFragment : Fragment() {
 
-//    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-//        homeViewModel =
-//                ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
         return root
     }
 }
--- app/src/main/java/com/example/y/bottomnav02/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/y/bottomnav02/ui/notifications/NotificationsFragment.kt
@@ -12,20 +12,14 @@ import com.example.y.bottomnav02.R
 
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
