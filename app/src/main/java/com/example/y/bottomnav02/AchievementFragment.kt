b122dfeb2ef3e1b64c5bbc4a39528cf80bdfb59a
YuSan357 <forestgreen14689@gmail.com>
2021-06-04 13:54:45 +0900

    ちょっと進めた

--- app/src/main/java/com/example/y/bottomnav02/AchievementFragment.kt
+++ app/src/main/java/com/example/y/bottomnav02/AchievementFragment.kt
@@ -1,12 +1,28 @@
 package com.example.y.bottomnav02
 
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.GridLayoutManager
+
 
 class AchievementFragment : Fragment() {
+
+
+    private lateinit var adapter:CustomRecyclerViewAdapter
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
 
     override fun onCreateView(
@@ -16,5 +32,19 @@ class AchievementFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_achievement, container, false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        //Todo: Realmからデータを取得
+        //Todo: RecyclerView表示
+    }
+
+
 
 }
--- app/src/main/java/com/example/y/bottomnav02/MainActivity.kt
+++ app/src/main/java/com/example/y/bottomnav02/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.y.bottomnav02
 
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -17,6 +18,13 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(applicationContext, "hello", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
 
 
