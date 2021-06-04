bf6da8a151168c9a3449937c4e8c9fbd57d06ec3
YuSan357 <forestgreen14689@gmail.com>
2021-06-04 16:52:52 +0900

    EditAchievementActivity.xmlを作成した。

---
+++ app/src/main/java/com/example/y/bottomnav02/EditAchievementActivity.kt
@@ -0,0 +1,11 @@
+package com.example.y.bottomnav02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAchievementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_achievement)
+    }
+}
