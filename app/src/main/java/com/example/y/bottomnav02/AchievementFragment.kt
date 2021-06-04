9d7c5ce41efb92b13b975529fc565d1ea2e77299
Yu357 <forestgreen14689@gmail.com>
2021-06-04 19:59:08 +0900

    kotlin-android-extensionsがimportされてなかったことによるエラーを解決した。

--- app/src/main/java/com/example/y/bottomnav02/AchievementFragment.kt
+++ app/src/main/java/com/example/y/bottomnav02/AchievementFragment.kt
@@ -1,17 +1,16 @@
 package com.example.y.bottomnav02
 
-import android.app.Application
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.fragment_achievement.*
+
+
+
 
 
 class AchievementFragment : Fragment() {
--- app/src/main/java/com/example/y/bottomnav02/MainActivity.kt
+++ app/src/main/java/com/example/y/bottomnav02/MainActivity.kt
@@ -17,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+
+
+
     }
 
 
