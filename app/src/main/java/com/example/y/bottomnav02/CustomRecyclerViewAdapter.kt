982bb8cc221f5f97ef4f55a8cd026c3f9a136712
YuSan357 <forestgreen14689@gmail.com>
2021-06-04 13:31:39 +0900

    おおよそ作成できた

---
+++ app/src/main/java/com/example/y/bottomnav02/CustomRecyclerViewAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.y.bottomnav02
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.RealmResults
+
+class CustomRecyclerViewAdapter(): RecyclerView.Adapter<ViewHolder>() {
+
+    //Todo: Realmを使用するため、rResultsを宣言
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.one_achievement, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return 9
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.titleText?.text = "The Forester"
+        holder.descriptionText?.text = "The Forestの世界で100日生き延びる"
+        holder.itemView.setOnClickListener {
+            //Todo: EditAchievementActivityへ遷移 putExtra付き
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/y/bottomnav02/ViewHolder.kt
@@ -0,0 +1,19 @@
+package com.example.y.bottomnav02
+
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.one_achievement.view.*
+
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var constraintLayout: ConstraintLayout? =null
+    var titleText: TextView? = null
+    var descriptionText: TextView? = null
+    init {
+        constraintLayout = itemView.constraintLayout
+        titleText = itemView.titleText
+        descriptionText = itemView.descriptionText
+    }
+
+}
