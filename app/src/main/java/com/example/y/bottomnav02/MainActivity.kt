965314166ebb10d26d98a1523c6476967cc76135
Yu357 <forestgreen14689@gmail.com>
2021-06-05 23:23:44 +0900

    ソフトウェアキーボード表示時に、レイアウトが押し上げられる問題を修正した

--- app/src/main/java/com/example/y/bottomnav02/MainActivity.kt
+++ app/src/main/java/com/example/y/bottomnav02/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.y.bottomnav02
 
 import android.os.Bundle
+import android.view.WindowManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+
+        //キーボードがレイアウトを押し上げないようにする
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
     }
 
 
