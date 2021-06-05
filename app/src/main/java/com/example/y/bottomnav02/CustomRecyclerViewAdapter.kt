2b46beb77f1d60d5732d8466fbe715420bf270df
Yu357 <forestgreen14689@gmail.com>
2021-06-05 15:38:58 +0900

    AchievementFragmentで、colorIdに応じてアチーブメントが色分けされるようになった。

--- app/src/main/java/com/example/y/bottomnav02/CustomRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/y/bottomnav02/CustomRecyclerViewAdapter.kt
@@ -1,15 +1,14 @@
 package com.example.y.bottomnav02
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.RealmResults
-import kotlin.coroutines.coroutineContext
 
 class CustomRecyclerViewAdapter(realmResults: RealmResults<Achievement>): RecyclerView.Adapter<ViewHolder>() {
 
@@ -17,9 +16,11 @@ class CustomRecyclerViewAdapter(realmResults: RealmResults<Achievement>): Recycl
     private val rResults: RealmResults<Achievement> = realmResults
 
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.one_achievement, parent, false)
+
         return ViewHolder(view)
     }
 
@@ -32,15 +33,46 @@ class CustomRecyclerViewAdapter(realmResults: RealmResults<Achievement>): Recycl
     @SuppressLint("ResourceAsColor")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
+        //contextを取得する
+        val context: Context = holder.itemView.context
+
         //Realmからデータを取得して、TextViewにセット
         val achievement = rResults[position]
         holder.titleText?.text = "${achievement?.title.toString()}"
         holder.descriptionText?.text = "${achievement?.description.toString()}"
 
-        //colorに応じて色を変更
-        holder.constraintLayout?.setBackgroundResource(R.drawable.background_border_green)
-        holder.titleText?.setTextColor(Color.parseColor("#C2FF7E"))
-        holder.descriptionText?.setTextColor(Color.parseColor("#C2FF7E"))
+        //colorIdに応じて色を変更する。0:white, 1:green, 2:blue, 3:purple, 4:orange, 5:gold
+        val colorId = achievement?.colorId
+        when (colorId) {
+            1L -> {
+                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_green)
+                holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.green))
+                holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.green))
+            }
+            2L -> {
+                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_blue)
+                holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.blue))
+                holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.blue))
+            }
+            3L -> {
+                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_purple)
+                holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.purple))
+                holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.purple))
+            }
+            4L -> {
+                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_orange)
+                holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.orange))
+                holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.orange))
+            }
+            5L -> {
+                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_gold)
+                holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.gold))
+                holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.gold))
+            }
+        }
+
+
+
 
         holder.itemView.setOnClickListener {
             val intent = Intent(it.context, EditAchievementActivity::class.java)
