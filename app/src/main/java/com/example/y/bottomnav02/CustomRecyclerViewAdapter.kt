1acfb1316b38ea0f11b2d287220068f32e71e1ef
Yu357 <forestgreen14689@gmail.com>
2021-06-05 19:24:25 +0900

    titleが空の場合でも、整って表示されるようになった。

--- app/src/main/java/com/example/y/bottomnav02/CustomRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/y/bottomnav02/CustomRecyclerViewAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.y.bottomnav02
 
 import android.annotation.SuppressLint
+import android.app.ActionBar
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.marginBottom
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.RealmResults
 
@@ -39,30 +42,38 @@ class CustomRecyclerViewAdapter(realmResults: RealmResults<Achievement>): Recycl
         holder.titleText?.text = achievement?.title.toString()
         holder.descriptionText?.text = achievement?.description.toString()
 
+        //もしtitleが空なら、titleTextは非表示。marginBottomも0dpにする。
+        if(holder.titleText?.text.isNullOrBlank()){
+            holder.titleText?.height = 0
+            val param = holder.titleText?.layoutParams as ViewGroup.MarginLayoutParams
+            param.setMargins(0,0,0,0)
+            holder.titleText?.layoutParams = param
+        }
+
         //colorIdに応じて色を変更する。0:white, 1:green, 2:blue, 3:purple, 4:orange, 5:gold
         when (achievement?.colorId) {
             1L -> {
-                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_green)
+                holder.linearLayout?.setBackgroundResource(R.drawable.background_achievement_green)
                 holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.green))
                 holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.green))
             }
             2L -> {
-                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_blue)
+                holder.linearLayout?.setBackgroundResource(R.drawable.background_achievement_blue)
                 holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.blue))
                 holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.blue))
             }
             3L -> {
-                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_purple)
+                holder.linearLayout?.setBackgroundResource(R.drawable.background_achievement_purple)
                 holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.purple))
                 holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.purple))
             }
             4L -> {
-                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_orange)
+                holder.linearLayout?.setBackgroundResource(R.drawable.background_achievement_orange)
                 holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.orange))
                 holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.orange))
             }
             5L -> {
-                holder.constraintLayout?.setBackgroundResource(R.drawable.background_achievement_gold)
+                holder.linearLayout?.setBackgroundResource(R.drawable.background_achievement_gold)
                 holder.titleText?.setTextColor(ContextCompat.getColor(context, R.color.gold))
                 holder.descriptionText?.setTextColor(ContextCompat.getColor(context, R.color.gold))
             }
