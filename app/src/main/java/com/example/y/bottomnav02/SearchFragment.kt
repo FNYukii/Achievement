857e6b398336cd977cfd9ea5361bef9c6e00a97f
Yu357 <forestgreen14689@gmail.com>
2021-06-05 22:59:06 +0900

    エンターキーを押すことで検索を実行できるようになった。

--- app/src/main/java/com/example/y/bottomnav02/SearchFragment.kt
+++ app/src/main/java/com/example/y/bottomnav02/SearchFragment.kt
@@ -1,9 +1,10 @@
 package com.example.y.bottomnav02
 
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -38,23 +39,35 @@ class SearchFragment : Fragment() {
         //Realmのデフォルトインスタンスを取得
         realm = Realm.getDefaultInstance()
 
-        searchButton.setOnClickListener {
-            searchText = searchEdit.text.toString()
-            showRecyclerView()
+
+        searchEdit.setOnKeyListener{ v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_ENTER){
+
+                //キーボードを閉じる
+//                val inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE)
+//                inputMethodManager.hideSoftInputFromWindow(v.windowToken, InputMethodManager.RESULT_UNCHANGED_SHOWN)
+
+                //検索してRecyclerView表示
+                searchText = searchEdit.text.toString()
+                runRecyclerView()
+
+                true
+            }
+            false
         }
+
 
     }
 
 
     override fun onStart() {
         super.onStart()
-
-        showRecyclerView()
-
+        runRecyclerView()
     }
 
 
-    private fun showRecyclerView(){
+    private fun runRecyclerView(){
+
         //Realmでレコードを検索
         val realmResults = realm.where(Achievement::class.java).findAll().sort("id", Sort.DESCENDING)
 
