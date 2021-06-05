1b621b44f6b51b3953b0add744c5c678b95f3c3a
Yu357 <forestgreen14689@gmail.com>
2021-06-05 11:28:08 +0900

    アチーブメントの色を変更する方法を試した。

--- app/src/main/java/com/example/y/bottomnav02/CustomRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/y/bottomnav02/CustomRecyclerViewAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.y.bottomnav02
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.RealmResults
+import kotlin.coroutines.coroutineContext
 
 class CustomRecyclerViewAdapter(realmResults: RealmResults<Achievement>): RecyclerView.Adapter<ViewHolder>() {
 
@@ -25,12 +29,18 @@ class CustomRecyclerViewAdapter(realmResults: RealmResults<Achievement>): Recycl
     }
 
 
+    @SuppressLint("ResourceAsColor")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         //Realmからデータを取得して、TextViewにセット
         val achievement = rResults[position]
         holder.titleText?.text = "${achievement?.title.toString()}"
         holder.descriptionText?.text = "${achievement?.description.toString()}"
+
+        //colorに応じて色を変更
+        holder.constraintLayout?.setBackgroundResource(R.drawable.background_border_green)
+        holder.titleText?.setTextColor(Color.parseColor("#C2FF7E"))
+        holder.descriptionText?.setTextColor(Color.parseColor("#C2FF7E"))
 
         holder.itemView.setOnClickListener {
             val intent = Intent(it.context, EditAchievementActivity::class.java)
