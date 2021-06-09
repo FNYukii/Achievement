2099d4867df6ca9de0ab9b8e691351d3f6972b75
Yu357 <forestgreen14689@gmail.com>
2021-06-09 23:27:51 +0900

    リファクタリング。ViewHolderクラスをCustomRecyclerViewAdapterクラス内に記述した。

--- app/src/main/java/com/example/y/achievement/CustomRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/y/achievement/CustomRecyclerViewAdapter.kt
@@ -5,13 +5,17 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import io.realm.*
-import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.one_frame.view.*
 
 class CustomRecyclerViewAdapter(
     private val collection: OrderedRealmCollection<Achievement>?
-    ) : RealmRecyclerViewAdapter<Achievement, CustomViewHolder>(collection, true) {
+    ) : RealmRecyclerViewAdapter<Achievement, CustomRecyclerViewAdapter.CustomViewHolder>(collection, true) {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
@@ -26,6 +30,15 @@ class CustomRecyclerViewAdapter(
     }
 
 
+    class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val frameBackground: ConstraintLayout = itemView.frameBackground
+        val framePinImage: ImageView = itemView.framePinImage
+        val frameAchieveImage: ImageView = itemView.frameAchieveImage
+        val frameTitleText: TextView = itemView.frameTitleText
+        val frameDetailText: TextView = itemView.frameDetailText
+    }
+
+
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
 
         //contextを取得する
@@ -36,60 +49,60 @@ class CustomRecyclerViewAdapter(
 
         //もしピン止めされたアチーブメントでないなら、ピンアイコンを非表示
         if(achievement?.isPinned == false){
-            holder.framePinImage?.visibility = View.GONE
+            holder.framePinImage.visibility = View.GONE
         }
 
         //もし、未達成のアチーブメントなら、達成アイコンを非表示
         if(achievement?.isAchieved == false){
-            holder.frameAchieveImage?.visibility = View.GONE
+            holder.frameAchieveImage.visibility = View.GONE
         }
 
         //タイトルと説明をセット
-        holder.frameTitleText?.text = achievement?.title.toString()
-        holder.frameDetailText?.text = achievement?.detail.toString()
+        holder.frameTitleText.text = achievement?.title.toString()
+        holder.frameDetailText.text = achievement?.detail.toString()
 
         //もしtitleがemptyなら、titleTextは非表示。detailTextのmarginTopも0dpにする。
         if(achievement?.title.isNullOrEmpty()){
-            holder.frameTitleText?.visibility = View.GONE
-            val param = holder.frameDetailText?.layoutParams as ViewGroup.MarginLayoutParams
+            holder.frameTitleText.visibility = View.GONE
+            val param = holder.frameDetailText.layoutParams as ViewGroup.MarginLayoutParams
             param.setMargins(0,0,0,0)
-            holder.frameDetailText?.layoutParams = param
+            holder.frameDetailText.layoutParams = param
         }
 
         //colorIdに応じて色を変更する。0:white, 1:green, 2:blue, 3:purple, 4:orange, 5:gold
         when (achievement?.colorId) {
             1 -> {
-                holder.frameBackground?.setBackgroundResource(R.drawable.background_frame_green)
-                holder.framePinImage?.setColorFilter(ContextCompat.getColor(context, R.color.green))
-                holder.frameAchieveImage?.setColorFilter(ContextCompat.getColor(context, R.color.green))
-                holder.frameTitleText?.setTextColor(ContextCompat.getColor(context, R.color.green))
-                holder.frameDetailText?.setTextColor(ContextCompat.getColor(context, R.color.green))
+                holder.frameBackground.setBackgroundResource(R.drawable.background_frame_green)
+                holder.framePinImage.setColorFilter(ContextCompat.getColor(context, R.color.green))
+                holder.frameAchieveImage.setColorFilter(ContextCompat.getColor(context, R.color.green))
+                holder.frameTitleText.setTextColor(ContextCompat.getColor(context, R.color.green))
+                holder.frameDetailText.setTextColor(ContextCompat.getColor(context, R.color.green))
             }
             2 -> {
-                holder.frameBackground?.setBackgroundResource(R.drawable.background_frame_blue)
-                holder.framePinImage?.setColorFilter(ContextCompat.getColor(context, R.color.blue))
-                holder.frameAchieveImage?.setColorFilter(ContextCompat.getColor(context, R.color.blue))
-                holder.frameTitleText?.setTextColor(ContextCompat.getColor(context, R.color.blue))
-                holder.frameDetailText?.setTextColor(ContextCompat.getColor(context, R.color.blue))
+                holder.frameBackground.setBackgroundResource(R.drawable.background_frame_blue)
+                holder.framePinImage.setColorFilter(ContextCompat.getColor(context, R.color.blue))
+                holder.frameAchieveImage.setColorFilter(ContextCompat.getColor(context, R.color.blue))
+                holder.frameTitleText.setTextColor(ContextCompat.getColor(context, R.color.blue))
+                holder.frameDetailText.setTextColor(ContextCompat.getColor(context, R.color.blue))
             }
             3 -> {
-                holder.frameBackground?.setBackgroundResource(R.drawable.background_frame_purple)
-                holder.framePinImage?.setColorFilter(ContextCompat.getColor(context, R.color.purple))
-                holder.frameAchieveImage?.setColorFilter(ContextCompat.getColor(context, R.color.purple))
-                holder.frameTitleText?.setTextColor(ContextCompat.getColor(context, R.color.purple))
-                holder.frameDetailText?.setTextColor(ContextCompat.getColor(context, R.color.purple))
+                holder.frameBackground.setBackgroundResource(R.drawable.background_frame_purple)
+                holder.framePinImage.setColorFilter(ContextCompat.getColor(context, R.color.purple))
+                holder.frameAchieveImage.setColorFilter(ContextCompat.getColor(context, R.color.purple))
+                holder.frameTitleText.setTextColor(ContextCompat.getColor(context, R.color.purple))
+                holder.frameDetailText.setTextColor(ContextCompat.getColor(context, R.color.purple))
             }
             4 -> {
-                holder.frameBackground?.setBackgroundResource(R.drawable.background_frame_gold)
-                holder.framePinImage?.setColorFilter(ContextCompat.getColor(context, R.color.gold))
-                holder.frameAchieveImage?.setColorFilter(ContextCompat.getColor(context, R.color.gold))
-                holder.frameTitleText?.setTextColor(ContextCompat.getColor(context, R.color.gold))
-                holder.frameDetailText?.setTextColor(ContextCompat.getColor(context, R.color.gold))
+                holder.frameBackground.setBackgroundResource(R.drawable.background_frame_gold)
+                holder.framePinImage.setColorFilter(ContextCompat.getColor(context, R.color.gold))
+                holder.frameAchieveImage.setColorFilter(ContextCompat.getColor(context, R.color.gold))
+                holder.frameTitleText.setTextColor(ContextCompat.getColor(context, R.color.gold))
+                holder.frameDetailText.setTextColor(ContextCompat.getColor(context, R.color.gold))
             }
         }
 
         //EditAchievementActivityへ遷移するクリックリスナーをセット
-        holder.frameBackground?.setOnClickListener {
+        holder.frameBackground.setOnClickListener {
             val intent = Intent(it.context, EditActivity::class.java)
             intent.putExtra("achievementId", achievement?.id)
             it.context.startActivity(intent)
